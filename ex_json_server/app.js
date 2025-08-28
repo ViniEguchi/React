99bb3b3c54ff6c48b29d79fac9e1ce3cbe86ded5
ViniEguchi <vinicius.eguchi@sptech.school>
2025-08-27 23:46:41 -0300

    feat: iniciando o exercicio sobre json_server

---
+++ ex_json_server/app.js
@@ -0,0 +1,30 @@
+async function show() {
+    
+}
+
+async function add() {
+    try {
+
+        show();
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+async function alter() {
+    try {
+
+        show();
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+async function del() {
+    try {
+        
+        show();
+    } catch (error) {
+        console.warn(error)
+    }
+}
