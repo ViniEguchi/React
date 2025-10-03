78b5ea0423b66a55856d1ce181aa34fde57d7d60
ViniEguchi <vinicius.eguchi@sptech.school>
2025-10-03 17:50:12 -0300

    feat: ex 4 useState

--- aula1react/src/components/ExibirPersonagens.jsx
+++ aula1react/src/components/ExibirPersonagens.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+
 export function ExibirPersonagens() {
+    const [personagens, setPersonagens] = useState([])
+
+    async function getPersonagens() {
+        const resposta = await fetch ("https://rickandmortyapi.com/api/character").then(res => res.json());
+        console.log(resposta)
+
+        const listaPersonagens = resposta.results.map(element => ({
+            nome: element.name,
+            imagem: element.image
+        }));
+
+        setPersonagens(listaPersonagens);
+    }
+
+    useEffect(() => {getPersonagens()}, [])
+
     return (
         <>
-            oi
+            <ul className="flex flex-col gap-4">
+                {personagens.map((personagem) => (
+                    <li>{personagem.nome} <img src={personagem.imagem}/></li>
+                ))}
+            </ul>
         </>
     )
 } 
